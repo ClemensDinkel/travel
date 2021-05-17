144655d92aa9bbb8358ddf4577e802e78e351003
jahanalem <s.r.alem19@gmail.com>
2021-05-18 01:17:31 +0200

    image

---
+++ app.js
@@ -0,0 +1,41 @@
+const countries = require('./routes/world');
+const express = require('express');
+const path = require('path');
+const countryRoute = require('./routes/countryRoute');
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+//app.use(express.static('public'))
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(countryRoute);
+
+
+//listen to
+
+app.listen(port, () => {
+    console.log(`server running on port ${port}`)
+})
+/* 
+app.post(
+    '/',
+    body('username').isEmail(),
+    // password must be at least 5 chars long
+    body('password').isLength({ min: 5 }),
+    (req, res) => {
+      // Finds the validation errors in this request and wraps them in an object with handy functions
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+  
+      User.create({
+        username: req.body.username,
+        password: req.body.password,
+      }).then(user => res.json(user));
+    },
+  ); */
